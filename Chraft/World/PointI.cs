d6a168eaa02d5f75518ef73d837fff62307425ae
spazzarama <justin.stenning@gmail.com>
2011-07-26 23:48:17 +0800

    Corrected PointI.Chunk/Block

--- Chraft/World/PointI.cs
+++ Chraft/World/PointI.cs
@@ -11,8 +11,8 @@ namespace Chraft.World
 		public readonly int X;
 		public readonly int Y;
 		public readonly int Z;
-		public PointI Chunk { get { return new PointI(X >> 4, Y >> 4); } }
-		public PointI Block { get { return new PointI(X << 4, Y << 4); } }
+		public PointI Chunk { get { return new PointI(X >> 4, Z >> 4); } }
+        public PointI Block { get { return new PointI(X << 4, Z << 4); } }
 
 		public PointI(int x, int y, int z)
 		{
