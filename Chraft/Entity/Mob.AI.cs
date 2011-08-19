38469426a35f9112e24a80214a23461f66ad7946
spazzarama <spazzarama@gmail.com>
2011-08-20 01:20:37 +1000

    Updated Vector3 class to make Mob.AI easier to work with (re angles, arrow deflection [vector reflection], and so on). See #4

--- Chraft/Entity/Mob.AI.cs
+++ Chraft/Entity/Mob.AI.cs
@@ -12,63 +12,73 @@ namespace Chraft.Entity {
         public bool Hunter; // Is this mob capable of tracking clients?
         public bool Hunting; // Is this mob currently tracking a client?
 
-        public void Update() {
+        public void Update()
+        {
             // TODO: Theory of Cosines to get direction heading from yaw or pitch.
 
-            if(true) // If to check if we've travelled in a direction long enough. Reset Velocity.
+            // TODO: confirm when is sine and which is cosine
+            //X = (float)Math.Cos(angle); // up is 0 and west(left) is Pi/2 for this
+            //Z = (float)Math.Sin(angle); // angle is radians
+
+            if (true) // If to check if we've travelled in a direction long enough. Reset Velocity.
                 Velocity = new Vector3(0, 0, 0); // Too lazy so mob is gonna be ADHD.
-       if(!AIWaiting)
-            switch (new Random().Next(1,5)) {
-                case 1:
-                    Velocity = new Vector3(1, 0, 0);
-                    break;
-                case 2:
-                    Velocity = new Vector3(-1, 0, 0);
-                    break;
-                case 3:
-                    Velocity = new Vector3(0, 0, 1);
-                    break;
-                case 4:
-                    System.Timers.Timer waitTimer = new System.Timers.Timer(new Random().Next(1, 5) * 1000);
-                    waitTimer.Elapsed += delegate {
-                        waitTimer.Stop();
-                        this.AIWaiting = false;
-                        waitTimer.Dispose();
-                    };
-                    this.AIWaiting = true;
-                    waitTimer.Start();
-                    break;
-                default:
-                    Velocity = new Vector3(0, 0, -1);
-                    break;
-            }
+            if (!AIWaiting)
+                switch (new Random().Next(1, 5))
+                {
+                    case 1:
+                        Velocity = new Vector3(1, 0, 0);
+                        break;
+                    case 2:
+                        Velocity = new Vector3(-1, 0, 0);
+                        break;
+                    case 3:
+                        Velocity = new Vector3(0, 0, 1);
+                        break;
+                    case 4:
+                        System.Timers.Timer waitTimer = new System.Timers.Timer(new Random().Next(1, 5) * 1000);
+                        waitTimer.Elapsed += delegate
+                        {
+                            waitTimer.Stop();
+                            this.AIWaiting = false;
+                            waitTimer.Dispose();
+                        };
+                        this.AIWaiting = true;
+                        waitTimer.Start();
+                        break;
+                    default:
+                        Velocity = new Vector3(0, 0, -1);
+                        break;
+                }
             // TODO: Actual collision prediction.
-        if (Velocity.Z != 0) {
-            if (World.GetBlockId((int)Position.X, (int)Position.Y, (int)(Position.Z + Velocity.Z)) != 0)
-                if (World.GetBlockId((int)Position.X, (int)Position.Y + 1, (int)(Position.Z + Velocity.Z)) != 0)
-                    Velocity.Z -= Velocity.Z;
-                else
-                    Velocity.Y += 1;
-        }
-        if (Velocity.X != 0) {
-            if (World.GetBlockId((int)(Position.X + Velocity.X), (int)Position.Y, (int)Position.Z) != 0)
-                if (World.GetBlockId((int)(Position.X + Velocity.X), (int)Position.Y + 1, (int)Position.Z) != 0)
-                    Velocity.X -= Velocity.X;
-                else
-                    Velocity.Y += 1;
-        }
+            if (Velocity.Z != 0)
+            {
+                if (World.GetBlockId((int)Position.X, (int)Position.Y, (int)(Position.Z + Velocity.Z)) != 0)
+                    if (World.GetBlockId((int)Position.X, (int)Position.Y + 1, (int)(Position.Z + Velocity.Z)) != 0)
+                        Velocity.Z -= Velocity.Z;
+                    else
+                        Velocity.Y += 1;
+            }
+            if (Velocity.X != 0)
+            {
+                if (World.GetBlockId((int)(Position.X + Velocity.X), (int)Position.Y, (int)Position.Z) != 0)
+                    if (World.GetBlockId((int)(Position.X + Velocity.X), (int)Position.Y + 1, (int)Position.Z) != 0)
+                        Velocity.X -= Velocity.X;
+                    else
+                        Velocity.Y += 1;
+            }
 
             // TODO: Actual gravity
-        if (World.GetBlockId((int)Position.X, (int)Position.Y - 1, (int)Position.Z) == 0)
-            Velocity.Y -= 1;
+            if (World.GetBlockId((int)Position.X, (int)Position.Y - 1, (int)Position.Z) == 0)
+                Velocity.Y -= 1;
 
             // Emergency Collision Detection
-        if (World.GetBlockId((int)(Position.X + Velocity.X),
-            (int)(Position.Y + Velocity.Y),
-            (int)(Position.Z + Velocity.Z)) != 0) {
-            // We're going straight into a block! Oh nooooooes.
+            if (World.GetBlockId((int)(Position.X + Velocity.X),
+                (int)(Position.Y + Velocity.Y),
+                (int)(Position.Z + Velocity.Z)) != 0)
+            {
+                // We're going straight into a block! Oh nooooooes.
                 Velocity.Y += 1;
-        }
+            }
 
             UpdatePosition();
         }
