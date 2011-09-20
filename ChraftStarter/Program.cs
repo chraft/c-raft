554138945bf772d031338cb4828be501d403c9ce
Christian W <seaboy1234@rakaienguard.com>
2011-09-20 01:02:45 -0700

    Changed the Chraft project to ChraftApi.  No real changes to C#raft itself. Add ChraftStarter.  All it does is load the Api. Plugin Developers: Use the "ChraftApi.dll" from now on. Closes #47.  About time.

---
+++ ChraftStarter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chraft;
+
+namespace ChraftStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Quick and esey workaround.
+            //Could make Chraft.Program a public static class and avoid this chain of one line methods.
+            Starter.Start(args);
+        }
+    }
+}
