556a45c82c31cb68e73b2725b79b26051518986f
Gremlin13 <DDFROW@gmail.com>
2011-10-10 19:23:05 +0700

    * Blocks can now be Spawned and Removed. Used when it's placed/destroyed by the server, not by the player. * Added basic block physics features - falling sand and gravel. See #52, #94

---
+++ Chraft/World/Blocks/Physics/FallingGravel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chraft.Utils;
+
+namespace Chraft.World.Blocks.Physics
+{
+    public class FallingGravel : FallingSand
+    {
+        public FallingGravel(WorldManager world, Location pos) : base(world, pos)
+        {
+            Type = Net.Packets.AddObjectVehiclePacket.ObjectType.FallingGravel;
+            BlockId = (byte) BlockData.Blocks.Gravel;
+        }
+    }
+}
