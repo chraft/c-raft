982325e7c56e1fa0f6b94f6a2d16ea70e2314e7a
Gremlin13 <DDFROW@gmail.com>
2011-10-15 07:23:02 +0700

    * Ice block transforms into water when it's destroyed

--- Chraft/World/Blocks/BlockIce.cs
+++ Chraft/World/Blocks/BlockIce.cs
@@ -15,6 +15,16 @@ namespace Chraft.World.Blocks
             Name = "Ice";
             Type = BlockData.Blocks.Ice;
             Opacity = 0x2;
+            IsSolid = true;
+        }
+
+        protected override void UpdateOnDestroy(StructBlock block)
+        {
+            block.World.SetBlockAndData(block.Coords, (byte)BlockData.Blocks.Still_Water, 0);
+            block.Chunk.RecalculateHeight(block.Coords);
+            block.Chunk.RecalculateSky(block.Coords.BlockX, block.Coords.BlockZ);
+            block.Chunk.SpreadSkyLightFromBlock((byte)(block.Coords.BlockX), (byte)block.Coords.BlockY, (byte)(block.Coords.BlockZ & 0xf));
+            block.World.Update(block.Coords, false);
         }
     }
 }
