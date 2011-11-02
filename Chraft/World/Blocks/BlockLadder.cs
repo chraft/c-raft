f776f8781855458cb6a37fe85d5c29123fbea193
Deniska <DeniskaDenisoff@gmail.com>
2011-11-02 17:09:22 +0700

    Fixed Ladder placement

--- Chraft/World/Blocks/BlockLadder.cs
+++ Chraft/World/Blocks/BlockLadder.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Chraft.Entity;
 using Chraft.Interfaces;
+using Chraft.Net;
 using Chraft.Plugins.Events.Args;
 
 namespace Chraft.World.Blocks
@@ -17,6 +18,28 @@ namespace Chraft.World.Blocks
             IsAir = true;
             LootTable.Add(new ItemStack((short)Type, 1));
             Opacity = 0x0;
+        }
+
+        public override void Place(EntityBase entity, StructBlock block, StructBlock targetBlock, BlockFace face)
+        {
+            switch (face)
+            {
+                case BlockFace.East:
+                    block.MetaData = (byte)MetaData.Ladders.East;
+                    break;
+                case BlockFace.West:
+                    block.MetaData = (byte)MetaData.Ladders.West;
+                    break;
+                case BlockFace.North:
+                    block.MetaData = (byte)MetaData.Ladders.North;
+                    break;
+                case BlockFace.South:
+                    block.MetaData = (byte)MetaData.Ladders.South;
+                    break;
+                default:
+                    return;
+            }
+            base.Place(entity, block, targetBlock, face);
         }
     }
 }
