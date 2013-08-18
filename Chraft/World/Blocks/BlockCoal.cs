6b1558460c0b9a411a8c04b1519d6c3485d98737
Dan <dan@danthekarman.tk>
2013-08-18 13:07:49 -0700

    Added coal block, glass is solid

---
+++ Chraft/World/Blocks/BlockCoal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chraft.World.Blocks.Base;
+using Chraft.Entity.Items;
+using Chraft.Utilities.Blocks;
+
+namespace Chraft.World.Blocks
+{
+    class BlockCoal : BlockBase
+    {
+        public BlockCoal()
+        {
+            Name = "Block of Coal";
+            Type = BlockData.Blocks.Block_Of_Coal;
+            IsSolid = true;
+            var item = ItemHelper.GetInstance((short)Type);
+            item.Count = 1;
+            LootTable.Add(item);
+        }
+    }
+}
